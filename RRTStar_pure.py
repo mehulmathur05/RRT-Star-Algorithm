edce65fcfe87a9e2d9ead77a8f4ab9d7b1d5867e
mehulmathur05 <mmehulmathur@gmail.com>
2024-05-02 17:41:41 +0530

    Refined the rewiring steps of the algorithm

--- RRTStar_pure.py
+++ RRTStar_pure.py
@@ -3,8 +3,10 @@ import cv2
 import numpy as np 
 import random
 
-image = cv2.imread("Image1.png")
+image = cv2.imread("Image2.png")
+image_ = image.copy()
 height, width, _ = np.shape(image)
+imageBin = np.zeros((height, width), np.uint8)
 class Node:
     def __init__(self, x, y):
         self.x = x
@@ -15,7 +17,7 @@ class Node:
 
     def setNeighbors(self, nodeList, neighborRadius):
         for node in nodeList:
-            print(node.getCoords())
+            # print(node.getCoords())
             if (distance(node.getCoords(), self.getCoords()) < neighborRadius and node not in self.neighbors):
                 self.neighbors.append(node)
                 node.neighbors.append(self)
@@ -32,35 +34,56 @@ class Node:
     def setCost(self, cost):
         self.cost = cost
 
-def drawPaths(start):
+    def removeChild(self, child):
+        self.children.remove(child)
+
+    def updateCosts(self, profit):
+        self.cost -= profit
+        for child in self.children:
+            child.updateCosts(profit)
+
+def drawPaths(start, image_):
     for node in start.children:
-        cv2.line(image, start.getCoords(), node.getCoords(), (255, 0, 0), 2)
-        drawPaths(node)
+        cv2.line(image_, start.getCoords(), node.getCoords(), (255, 0, 0), 2)
+        image_ = drawPaths(node, image_)
+    return image_
 
-def retracePath(goal, start):
-    if goal == start: return
-    cv2.line(image, goal.getCoords(), goal.parent.getCoords(), (0, 255, 0), 3)
-    retracePath(goal.parent, start)
+def retracePath(goal, start, image_):
+    if goal == start: return image_
+    cv2.line(image_, goal.getCoords(), goal.parent.getCoords(), (0, 255, 0), 3)
+    image_ = retracePath(goal.parent, start, image_)
+    return image_
 
-for i in range(height):
-    for j in range(width):
-        if image[i][j][0] == 76 and image[i][j][1] == 177 and image[i][j][2] == 34:
-            startNode = Node(j, i)
-        elif image[i][j][0] == 36 and image[i][j][1] == 28 and image[i][j][2] == 237:
-            goalNode = Node(j, i)
+def setStartEnd():
+    countStart = 0; countEnd = 0
+    start = (0,0)
+    goal = (0,0)
+    for i in range(height):
+        for j in range(width):
+            if image[i][j][0] == 76 and image[i][j][1] == 177 and image[i][j][2] == 34:
+                start = (start[0] + j , start[1] + i)
+                countStart+=1
+            elif image[i][j][0] == 36 and image[i][j][1] == 28 and image[i][j][2] == 237:
+                goal = (goal[0] + j, goal[1] + i)
+                countEnd+=1
+    start = (int(start[0]/countStart), int(start[1]/countStart))
+    goal = (int(goal[0]/countEnd), int(goal[1]/countEnd))
+    startNode = Node(*start)
+    goalNode = Node(*goal)
+    return startNode, goalNode
+
 
 def distance(coord1, coord2):
     dist = math.sqrt(pow((coord1[0] - coord2[0]), 2) + pow((coord1[1] - coord2[1]), 2))
     return dist
 
-image_ = np.zeros((height, width), np.uint8)
 def feasible(image):
     feasiblePoints = []
     for i in range(height):
         for j in range(width):
             if any(image[i][j] != 0):
                 feasiblePoints.append((j, i))
-                image_[i][j] = 255
+                imageBin[i][j] = 255
 
     return feasiblePoints
 
@@ -82,10 +105,10 @@ def checkObstacle(coord1, coord2):
                 
             for i in range(coord1[0], coord2[0]-1):
                 j = math.floor(slope*i) + math.floor(intercept)
-                print(coord1, coord2, (i, j))
+                # print(coord1, coord2, (i, j))
                 if (i, j) == coord2:
                     return False
-                elif image_[j,i] == 0:
+                elif imageBin[j,i] == 0:
                     return True
        
         else:
@@ -96,10 +119,10 @@ def checkObstacle(coord1, coord2):
                 
             for j in range(coord1[1], coord2[1]-1):
                 i = (j-intercept)/slope
-                print(coord1, coord2, (i, j))
+                # print(coord1, coord2, (i, j))
                 if (i, j) == coord2:
                     return False
-                if image_[j,math.floor(i)] == 0:
+                if imageBin[j,math.floor(i)] == 0:
                     return True
 
     if coord1[0] == coord2[0]:
@@ -109,7 +132,7 @@ def checkObstacle(coord1, coord2):
             coord2 = temp
         i = coord1[0]
         for j in range(coord1[1], coord2[1]-1):
-            if image_[j,i] == 0:
+            if imageBin[j,i] == 0:
                 return True
             # elif (i,j) == coord2:
             #     return False  
@@ -119,7 +142,7 @@ def nearestNode(randPt, nodeList):
     minDist = 1e5
     found = False
     for node in nodeList:
-        print(randPt, node.getCoords())
+        # print(randPt, node.getCoords())
         if not checkObstacle(randPt, node.getCoords()):
             dist = distance(randPt, node.getCoords())
             if dist < minDist:
@@ -131,8 +154,11 @@ def nearestNode(randPt, nodeList):
 
 
 def RRTStar(image, startNode, goalNode):
-    nodeList = [startNode]
     feasiblePoints = feasible(image)
+    nodeList = [startNode]
+    goalNode.cost = 1e5
+    pathFound = False
+    count = 0
     while True:
         randPt = randomPoint(feasiblePoints)
 
@@ -140,7 +166,6 @@ def RRTStar(image, startNode, goalNode):
 
         nearest, dNearest = nearestNode(randPt, nodeList)
         if (dNearest == 0): 
-            # print("Jj")
             continue
         # else: cv2.circle(image, randPt, 2, (0,255,0), -1)
 
@@ -166,23 +191,48 @@ def RRTStar(image, startNode, goalNode):
         newNode.setParent(father)
         father.setChild(newNode)
 
-        if distance(newNode.getCoords(), goalNode.getCoords()) < stepDist:
-            nodeList.append(goalNode)
-            newNode.setNeighbors(nodeList, 2.5*stepDist)
-            goalNode.setCost(newNode.cost + distance(newNode.getCoords(), goalNode.getCoords()))
-            goalNode.setParent(newNode)
-            newNode.setChild(goalNode)
-            retracePath(goalNode, startNode)
-            break 
 
-        drawPaths(startNode)
-        cv2.imshow("image", image)
-        cv2.waitKey(1)
-        cv2.destroyAllWindows()
+        if distance(newNode.getCoords(), goalNode.getCoords()) < 2.5*stepDist:
+            if not pathFound:
+                nodeList.append(goalNode)
+                pathFound = True
+                goalNode.setParent(newNode)
+                goalNode.setNeighbors(nodeList, 2.5*stepDist)
+                newNode.setChild(goalNode)
 
+        # REWIRING
+        for neighbor in newNode.neighbors:
+            if (not checkObstacle(newNode.getCoords(), neighbor.getCoords())):
+                cost = newNode.cost + distance(newNode.getCoords(), neighbor.getCoords())
+                if cost < neighbor.cost:
+                    neighbor.parent.removeChild(neighbor)
+                    neighbor.setParent(newNode)
+                    newNode.setChild(neighbor)
+                    # neighbor.setCost(cost)
+                    profit = neighbor.cost - cost
+                    neighbor.updateCosts(profit)
+            # image_ = image.copy()
+            # image_ = drawPaths(startNode, image_)
+            # image_ = retracePath(goalNode, startNode, image_)
+            #break
 
-RRTStar(image, startNode, goalNode)
+        image_ = image.copy()
+        image_ = drawPaths(startNode, image_)
+        if pathFound:
+            image_ = retracePath(goalNode, startNode, image_)
+        if not count % 10:
+            cv2.imshow("image", image_)
+            k = cv2.waitKey(10) & 0xFF
+            if k == 27:
+                break
+            cv2.destroyAllWindows()
+        count +=1 
+    return image_
 
-cv2.imshow("image", image)
+startNode, goalNode = setStartEnd()
+image_ = RRTStar(image, startNode, goalNode)
+# print(image_)
+
+cv2.imshow("image", image_)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
